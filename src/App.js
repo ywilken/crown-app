f646ea0b0d7bcc08c6f7a23c72d3afbe034ff17b
Yannick Wilken <61846545+ywilken@users.noreply.github.com>
2020-05-18 05:42:31 -0500

    updating our app with shop-page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 // Now we import the Route Component
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import { HomePage } from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -10,17 +11,17 @@ function App() {
       {/* <HomePage/> */}
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
 }
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// )
 
 export default App;
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component.jsx';
 
-export const HomePage = (props) => (
+const HomePage = (props) => (
     console.log(props),
     <div className='homepage'>
         <Directory/>
     </div>
 );
 
+export default HomePage;
